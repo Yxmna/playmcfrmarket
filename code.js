e8db833cb78dbe8d5896c0ba2532c3b77d1d03b2
Yomna <57047086+Yxmna@users.noreply.github.com>
2021-01-16 20:45:52 +0100

    Update

--- code.js
+++ code.js
@@ -5,15 +5,23 @@ var prices = [];
 var shops = [];
 var types = [];
 var fr = new Object;
+var en = new Object;
+var effects = ["Potion", "Tipped Arrow", "Splash Potion"];
+var tools = ["Sword", "Axe", "Shovel", "Hoe", "Trident", "Bow", "Crossbow", "Pickaxe", "Fishing Rod", "Shears", "Turtle Helmet", "Elytra"];
+
 console.log("version: " + version);
 
-fetch("https://yxmna.github.io/mcapi/lang/fr_FR.json").then(function(response) {
+fetch("https://yxmna.github.io/mcapi/lang/fr_fr_simply.json").then(function(response) {
   return response.json();
 }).then(function(obj) {
   fr = obj;
 });
 
-
+fetch("https://yxmna.github.io/mcapi/lang/en_us_simply.json").then(function(response) {
+  return response.json();
+}).then(function(obj) {
+  en = obj;
+});
 
 fetch(url)
   .then(function(res) {
@@ -25,37 +33,58 @@ fetch(url)
     //   return a.gsx$ID.$t - b.gsx$ID.$t;
     // });
     base.sort(function(a, b) {
-      if (a.gsx$nomproduit.$t.toUpperCase() < b.gsx$nomproduit.$t.toUpperCase()) return -1;
-      if (a.gsx$nomproduit.$t.toUpperCase() > b.gsx$nomproduit.$t.toUpperCase()) return 1;
+      if (fr[a.gsx$produit.$t.split(" ").join("_").toLowerCase()] < fr[b.gsx$produit.$t.split(" ").join("_").toLowerCase()]) return -1;
+      if (fr[a.gsx$produit.$t.split(" ").join("_").toLowerCase()] > fr[b.gsx$produit.$t.split(" ").join("_").toLowerCase()]) return 1;
+      // if (a.gsx$id.$t < b.gsx$id.$t) return -1;
+      // if (a.gsx$id.$t > b.gsx$id.$t) return 1;
       return 0;
     });
     pre();
     load();
   });
 
+
+
+// --------------------------------------------------------------------------------------------------------PRELOAD
 function pre() {
+  document.getElementById("search").value = "";
   base.forEach((item, i) => {
     var img = new Image();
-    img.src = "https://yxmna.github.io/mcapi/img/" + item.gsx$productname.$t.toLowerCase().split(" ").join("_") + ".png";
-    img.src = "https://yxmna.github.io/mcapi/img/" + item.gsx$productname.$t.toLowerCase().split(" ").join("_") + ".png";
+    console.log(item.gsx$produit.$t);
+    if (String(tools).includes(item.gsx$produit.$t.split(" ").pop()) && item.gsx$caracteristique.$t) {
+      img.src = "https://yxmna.github.io/mcapi/img/enchanted_" + en[item.gsx$produit.$t.toLowerCase().split(" ").join("_")].split(" ").join("_").toLowerCase() + ".png";
+    } else {
+      img.src = "https://yxmna.github.io/mcapi/img/" + en[item.gsx$produit.$t.toLowerCase().split(" ").join("_")].split(" ").join("_").toLowerCase() + ".png";
+    }
     img.onerror = function() {
-      img.src = "https://yxmna.github.io/mcapi/img/missing.png";
+      // img.src = "https://yxmna.github.io/mcapi/img/missing.png";
     };
     imgs[item.gsx$id.$t] = img;
   });
 }
 
+
+// --------------------------------------------------------------------------------------------------------LOAD
 function load(name) {
   document.getElementById("items").innerHTML = "";
 
   prices, types, shops = [];
   var db = base;
   if (name) db = db.filter(item => {
-    if (item.gsx$nomproduit.$t.toLowerCase().includes(name) || item.gsx$productname.$t.toLowerCase().includes(name)) return item;
+    if (fr[item.gsx$produit.$t.toLowerCase().split(" ").join("_")]) {
+      if (fr[item.gsx$produit.$t.toLowerCase().split(" ").join("_")].toLowerCase().includes(name) || item.gsx$produit.$t.toLowerCase().includes(name)) return item;
+    } else {
+      if (item.gsx$produit.$t.toLowerCase().includes(name)) return item;
+    }
   });
+
   if (document.getElementById("prices").value) db = db.filter(item => item.gsx$nomprix.$t.includes(document.getElementById("prices").value));
   if (document.getElementById("shops").value) db = db.filter(item => item.gsx$commerce.$t.includes(document.getElementById("shops").value));
   if (document.getElementById("types").value) db = db.filter(item => item.gsx$typevente.$t.includes(document.getElementById("types").value));
+
+
+
+
 
 
   db.forEach((item, i) => {
@@ -63,19 +92,12 @@ function load(name) {
     if (!types.includes(item.gsx$typevente.$t) && item.gsx$typevente.$t) {
       types.push(item.gsx$typevente.$t);
     }
-
     if (!shops.includes(item.gsx$commerce.$t) && item.gsx$commerce.$t) {
       shops.push(item.gsx$commerce.$t);
     }
-
-
     if (!prices.includes(item.gsx$nomprix.$t) && item.gsx$nomprix.$t) {
       prices.push(item.gsx$nomprix.$t);
     }
-
-
-
-
 
     var article = document.createElement("article");
     var img = document.createElement("img");
@@ -87,25 +109,57 @@ function load(name) {
     var br = document.createElement("br");
 
     img = imgs[item.gsx$id.$t];
-    // img.src = "https://yxmna.github.io/mcapi/img/" + item.gsx$productname.$t.toLowerCase().split(" ").join("_") + ".png";
-    // img.onerror = function() {
-    //   img.src = "https://yxmna.github.io/mcapi/img/missing.png";
-    // };
+
+    if (effects.includes(item.gsx$produit.$t) && item.gsx$caracteristique) {
+      var extra1 = item.gsx$caracteristique.$t.split(" ");
+      var extra2 = "";
+      if (extra1.length > 1) {
+        extra2 = extra1.pop();
+      }
+      title.innerHTML = item.gsx$quantiteproduit.$t + " " + fr[item.gsx$produit.$t.split(" ").join("_").toLowerCase() + ".effect." + extra1.join("_").toLowerCase()] + " " + extra2;
+
+    } else if (tools.includes(item.gsx$produit.$t) && item.gsx$caracteristique) {
 
 
 
-    title.innerHTML = item.gsx$quantiteproduit.$t + " " + item.gsx$nomproduit.$t;
 
+
+    } else if ("Filled Map".includes(item.gsx$produit.$t) && item.gsx$caracteristique) {
+
+      var extra1 = item.gsx$caracteristique.$t.split(" ");
+      var extra2 = extra1.pop();
+      if (!extra2.startsWith("https://")) {
+        extra1 = extra1.join(" ") + String(extra2);
+        extra2 = "";
+      }
+
+
+      console.log(extra1);
+      console.log(extra2);
+      title.innerHTML = item.gsx$quantiteproduit.$t + " " + fr[item.gsx$produit.$t.split(" ").join("_").toLowerCase()] + " " + item.gsx$caracteristique.$t;
+
+
+
+
+
+
+
+    } else {
+      if (fr[item.gsx$produit.$t.split(" ").join("_").toLowerCase()]) {
+        title.innerHTML = item.gsx$quantiteproduit.$t + " " + fr[item.gsx$produit.$t.split(" ").join("_").toLowerCase()];
+      } else {
+        title.innerHTML = item.gsx$quantiteproduit.$t + " *" + item.gsx$produit.$t;
+      }
+    }
 
     line1.innerHTML = item.gsx$commerce.$t;
     line2.innerHTML = item.gsx$proprietaire.$t + ", " + item.gsx$typevente.$t.toLowerCase();
-
 
     if (item.gsx$quantiteprix.$t == 0) {
       price.innerHTML = "Gratuit";
     } else {
       price.innerHTML = item.gsx$quantiteprix.$t + " ";
-      img_price.src = "https://yxmna.github.io/mcapi/img/" + item.gsx$nameprice.$t.toLowerCase().split(" ").join("_") + ".png";
+      img_price.src = "https://yxmna.github.io/mcapi/img/" + en[item.gsx$nameprice.$t.toLowerCase().split(" ").join("_")].split(" ").join("_").toLowerCase() + ".png";
       price.appendChild(img_price);
       price.innerHTML = price.innerHTML + " " + item.gsx$nomprix.$t;
     }
@@ -119,6 +173,8 @@ function load(name) {
       click(this.id);
     }
 
+    article.classList.add(item.gsx$produit.$t.split(" ").join("_").toLowerCase());
+
     article.appendChild(img);
     article.appendChild(br);
     article.appendChild(title);
@@ -127,13 +183,9 @@ function load(name) {
     article.appendChild(line2);
 
     document.getElementById("items").appendChild(article);
-
-
-
   });
 
   if (document.getElementById("prices").length == 1) {
-
     prices.forEach((item, i) => {
       var option = document.createElement("option");
       option.innerHTML = item;
@@ -161,6 +213,7 @@ function search() {
 }
 
 
+// --------------------------------------------------------------------------------------------------------CLICK
 function click(x) {
   console.log(x);
 }
